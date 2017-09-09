fde146af8698f777b27dc7a64cec5a1e93ac200b
CARLOS ROBERTO CHAVARRIA VEGA <carlos.chavarria7@ulatina.net>
2017-09-09 16:09:55 -0600

    Se crea el sitio base para el calentamiento del curso

---
+++ Ulatina.Electiva.Calentamiento/Ulatina.Electiva.Calentamiento/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ulatina.Electiva.Calentamiento.MvcApplication" Language="C#" %>
---
+++ Ulatina.Electiva.Calentamiento/Ulatina.Electiva.Calentamiento/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ulatina.Electiva.Calentamiento.Startup))]
+namespace Ulatina.Electiva.Calentamiento
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
