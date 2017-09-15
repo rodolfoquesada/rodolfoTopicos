2f24a6d11016e368b9156344f13cd81991450716
CARLOS ROBERTO CHAVARRIA VEGA <carlos.chavarria7@ulatina.net>
2017-09-14 21:37:27 -0600

    Se creo el menú de consola y la clase jugador

--- Ulatina.Electiva.Calentamiento/GatoGenerico/Program.cs
+++ Ulatina.Electiva.Calentamiento/GatoGenerico/Program.cs
@@ -10,11 +10,19 @@ namespace GatoGenerico
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+            Console.WriteLine("Nombre del primer jugador:");
+            String nombre1 = Console.ReadLine();
+            Console.WriteLine("Nombre del segundo jugador:");
+            String nombre2 = Console.ReadLine();
+            Console.WriteLine("Primero en jugar? Digite:");
+            Console.WriteLine("1. Jugador con 'X'");
+            Console.WriteLine("2. Jugador con '0'");
+            int PrimeroJugar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Seleccione el tamaño de la cuadricula:");
+            Console.WriteLine("1. Tamaño de 3X3");
+            Console.WriteLine("2. Tamaño de 4X4");
+            Console.WriteLine("3. Tamaño de 5X5");
+            int Tamaño = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
