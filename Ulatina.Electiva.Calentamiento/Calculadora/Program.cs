fc44e48bc82d95557d4804bb80afc3391cd86635
CARLOS ROBERTO CHAVARRIA VEGA <carlos.chavarria7@ulatina.net>
2017-09-16 15:21:05 -0600

    Se agregó un nuevo proyecto

---
+++ Ulatina.Electiva.Calentamiento/Calculadora/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+            Console.WriteLine("BIENVENIDO A LA CALCULADORA");
+            Console.WriteLine("");
+            Console.WriteLine("Digite la operacion que desea realizar");
+            String operacion = Console.ReadLine();
+            Console.WriteLine($"El resultado es: {res}");
+            Console.ReadLine(); */
+            Console.WriteLine("BIENVENIDO A LA CALCULADORA");
+            Console.WriteLine("");
+            Console.WriteLine("Digite la operacion que desea realizar");
+            String operacion = Console.ReadLine();
+        }
+    }
+}
