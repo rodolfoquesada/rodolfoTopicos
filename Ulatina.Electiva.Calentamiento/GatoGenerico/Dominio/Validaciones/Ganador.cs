6fe1322e6c76a2f068c599035ec9c5f47ce3a4ae
Rodolfo Quesada Amores <fofo.q.a@hotmail.com>
2017-09-15 18:20:32 -0600

    Se agregaron validaciones de verticales y diagonales

--- Ulatina.Electiva.Calentamiento/GatoGenerico/Dominio/Validaciones/Ganador.cs
+++ Ulatina.Electiva.Calentamiento/GatoGenerico/Dominio/Validaciones/Ganador.cs
@@ -11,7 +11,15 @@ namespace GatoGenerico.Dominio.Validaciones
     {
         public static Boolean ValidarGanador(String[] tablero, Jugador pJugador, Juego pJuego)
         {
-           if( ValidarHorizontales(tablero, pJugador, pJuego))
+            if (ValidarHorizontales(tablero, pJugador, pJuego))
+            {
+                return true;
+            }
+            else if (ValidarVerticales(tablero, pJugador, pJuego))
+            {
+                return true;
+            }
+            else if (ValidarDiagonales(tablero, pJugador, pJuego))
             {
                 return true;
             }
@@ -106,10 +114,147 @@ namespace GatoGenerico.Dominio.Validaciones
             return false;
         }
 
-        /*private static Boolean ValidarVerticales(String[] tablero, Jugador pJugador, Juego pJuego)
+        private static Boolean ValidarVerticales(String[] tablero, Jugador pJugador, Juego pJuego)
         {
-            
+            if (pJuego.GetTamanoTablero().Equals(1))
+            {
+                if (tablero[0] == pJugador.GetFichaJugador() && tablero[3] == pJugador.GetFichaJugador()
+                    && tablero[6] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[1] == pJugador.GetFichaJugador() && tablero[4] == pJugador.GetFichaJugador()
+                   && tablero[7] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[2] == pJugador.GetFichaJugador() && tablero[5] == pJugador.GetFichaJugador()
+                    && tablero[8] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+            }
+            else if (pJuego.GetTamanoTablero().Equals(2))
+            {
+                if (tablero[0] == pJugador.GetFichaJugador() && tablero[4] == pJugador.GetFichaJugador()
+                    && tablero[8] == pJugador.GetFichaJugador() && tablero[12] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[1] == pJugador.GetFichaJugador() && tablero[5] == pJugador.GetFichaJugador()
+                    && tablero[9] == pJugador.GetFichaJugador() && tablero[13] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[2] == pJugador.GetFichaJugador() && tablero[6] == pJugador.GetFichaJugador()
+                    && tablero[10] == pJugador.GetFichaJugador() && tablero[14] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[3] == pJugador.GetFichaJugador() && tablero[7] == pJugador.GetFichaJugador()
+                    && tablero[11] == pJugador.GetFichaJugador() && tablero[15] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+            }
+            else if (pJuego.GetTamanoTablero().Equals(3))
+            {
+                if (tablero[0] == pJugador.GetFichaJugador() && tablero[5] == pJugador.GetFichaJugador()
+                    && tablero[10] == pJugador.GetFichaJugador() && tablero[15] == pJugador.GetFichaJugador()
+                    && tablero[20] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[1] == pJugador.GetFichaJugador() && tablero[6] == pJugador.GetFichaJugador()
+                    && tablero[11] == pJugador.GetFichaJugador() && tablero[16] == pJugador.GetFichaJugador()
+                    && tablero[21] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[2] == pJugador.GetFichaJugador() && tablero[7] == pJugador.GetFichaJugador()
+                    && tablero[12] == pJugador.GetFichaJugador() && tablero[17] == pJugador.GetFichaJugador()
+                    && tablero[22] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[3] == pJugador.GetFichaJugador() && tablero[8] == pJugador.GetFichaJugador()
+                    && tablero[13] == pJugador.GetFichaJugador() && tablero[18] == pJugador.GetFichaJugador()
+                    && tablero[23] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[4] == pJugador.GetFichaJugador() && tablero[9] == pJugador.GetFichaJugador()
+                    && tablero[14] == pJugador.GetFichaJugador() && tablero[19] == pJugador.GetFichaJugador()
+                    && tablero[24] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+            }
+            return false;
         }
-        */
+
+        private static Boolean ValidarDiagonales(String[] tablero, Jugador pJugador, Juego pJuego)
+        {
+            if (pJuego.GetTamanoTablero().Equals(1))
+            {
+                if (tablero[0] == pJugador.GetFichaJugador() && tablero[4] == pJugador.GetFichaJugador()
+                    && tablero[8] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[2] == pJugador.GetFichaJugador() && tablero[4] == pJugador.GetFichaJugador()
+                   && tablero[6] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+            }
+            else if (pJuego.GetTamanoTablero().Equals(2))
+            {
+                if (tablero[0] == pJugador.GetFichaJugador() && tablero[5] == pJugador.GetFichaJugador()
+                    && tablero[10] == pJugador.GetFichaJugador() && tablero[15] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[3] == pJugador.GetFichaJugador() && tablero[6] == pJugador.GetFichaJugador()
+                    && tablero[9] == pJugador.GetFichaJugador() && tablero[12] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+            }
+            else if (pJuego.GetTamanoTablero().Equals(3))
+            {
+                if (tablero[0] == pJugador.GetFichaJugador() && tablero[6] == pJugador.GetFichaJugador()
+                    && tablero[12] == pJugador.GetFichaJugador() && tablero[18] == pJugador.GetFichaJugador()
+                    && tablero[24] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+                else if (tablero[4] == pJugador.GetFichaJugador() && tablero[8] == pJugador.GetFichaJugador()
+                    && tablero[12] == pJugador.GetFichaJugador() && tablero[16] == pJugador.GetFichaJugador()
+                    && tablero[20] == pJugador.GetFichaJugador())
+                {
+                    pJuego.SetGanadorJuego(pJugador.GetJugada());
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
